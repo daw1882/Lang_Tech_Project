0c8d39183767d615a0d0e5fb35b87b5ead643279
daw1882 <daw1882@g.rit.edu>
2019-10-12 22:06:36 -0400

    Changed the recordings have the same frame size as the samples

--- analyze_audio.py
+++ analyze_audio.py
@@ -2,7 +2,7 @@ from python_speech_features import mfcc
 from python_speech_features import logfbank
 import scipy.io.wavfile as wav
 
-(rate, sig) = wav.read("OAF_youth_happy.wav")
+(rate, sig) = wav.read("output.wav")
 mfcc_feat = mfcc(sig, rate, nfft=610)
 fbank_feat = logfbank(sig, rate, nfft=610)
 
--- record_audio.py
+++ record_audio.py
@@ -6,7 +6,7 @@ import time
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
-fs = 44100  # Record at 44100 samples per second
+fs = 12200  # Record at 44100 samples per second
 seconds = 3
 filename = "output.wav"
 
