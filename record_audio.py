24113d69d2a8eb36d8d18d14860c360b4f1c6da5
daw1882 <daw1882@rit.edu>
2019-11-16 12:42:07 -0500

    currently working, using wrong input data

--- record_audio.py
+++ record_audio.py
@@ -50,3 +50,34 @@ wf.setsampwidth(p.get_sample_size(sample_format))
 wf.setframerate(fs)
 wf.writeframes(b''.join(frames))
 wf.close()
+
+from keras.models import load_model
+import numpy as np
+import librosa
+from keras.models import model_from_json
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+loaded_model.load_weights("model/model.h5")
+print("Loaded model from disk")
+
+
+def predict(audio):
+    prob = loaded_model.predict(audio)
+    index = np.argmax(prob[0])
+    return index
+
+
+X, sample_rate = librosa.load('output.wav', res_type='kaiser_fast', duration=3,
+                              sr=22050, offset=0.5)
+sample_rate = np.array(sample_rate)
+mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13), axis=0)
+feature = mfccs
+
+predict(mfccs)
+
+
+
