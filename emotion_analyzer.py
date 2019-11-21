fea5f8e1e1d584ad540f04a41ce4552f6c02931a
daw1882 <daw1882@rit.edu>
2019-11-20 22:01:23 -0500

    Change emotion analyzer to set default array size based on model and added some tests to try out

--- emotion_analyzer.py
+++ emotion_analyzer.py
@@ -39,7 +39,7 @@ def get_emotions(filename, model):
     df = pd.DataFrame(data['feature'].values.tolist())
     df = df.fillna(0)
 
-    if model == 'TESS_model':
+    if model == 'TESS_model' or model == 'RAVDESS_model':
         test = np.zeros((1, 215))
     else:
         test = np.zeros((1, 259))
